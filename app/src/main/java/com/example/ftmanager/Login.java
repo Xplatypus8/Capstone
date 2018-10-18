b3d29f554ab4ca5e19460d5473b1188283636d4b
Clifton Dent <clifton.dent.15@cnu.edu>
2018-10-18 11:46:27 -0400

    Added login functionality and Funt Time logo

--- app/src/main/java/com/example/ftmanager/Login.java
+++ app/src/main/java/com/example/ftmanager/Login.java
@@ -2,12 +2,24 @@ package com.example.ftmanager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class Login extends AppCompatActivity {
-
+    private EditText usernameET, passwordET;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        usernameET = (EditText)findViewById(R.id.username);
+        passwordET = (EditText)findViewById(R.id.password);
+    }
+
+    public void login(View view){
+        String username = usernameET.getText().toString();
+        String password = passwordET.getText().toString();
+        String type = "login";
+        DatabaseConnector dbConnect = new DatabaseConnector(this);
+        dbConnect.execute(type, username, password );
     }
 }
