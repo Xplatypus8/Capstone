e90d2e23c5ca6c7c419996eaf89bf382b6516f14
Clifton Dent <clifton.dent.15@cnu.edu>
2018-11-18 14:26:48 -0500

    Created ViewDetailsActivity

---
+++ app/src/main/java/com/example/ftmanager/ViewReportDetails.java
@@ -0,0 +1,13 @@
+package com.example.ftmanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewReportDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_report_details);
+    }
+}
